b5315478b7b05fc2e2880097df83b44cc14283d8
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-10-15 15:49:26 +0200

    not the end but still good

--- clicker.js
+++ clicker.js
@@ -2,7 +2,6 @@ var count=1;
 
 $("document").ready(function() {
 
-
   $("button").click(function() {
 
       count++;
@@ -12,22 +11,36 @@ $("document").ready(function() {
       $(".count-leave").addClass("count-leave-active");
       setTimeout(function() {
         $(".count-leave").remove();
-      }.1000);
+      },1000);
 
 // enter
 
       $(".count-enter").addClass("count-enter-active");
       setTimeout(function() {
         $(".count-enter").addClass("count-leave").removeClass('count-enter count-enter-active');
-              $("body").append("<h1 class="count count-enter">"+ count +"</h1>")
-      }.1000);
-
-
-
-
+              $("body").append("<h1 class='count count-enter'>"+ count +"</h1>");
+      },1000);
 
   });
 
+});
 
-
-})
+// idee de tableau a bosser chef
+// var data = [
+//   {nam:"", score:""}
+// ];
+// ++;
+//
+// data.sort(function(a,b){
+//   if(a.score > b.score)
+//   return -1;
+//   else(a.scrore < b.score)
+//   return +1;
+// })
+//
+// for(var i in data){
+//   if(i != 0 && data[i].score == data[i-1].score)
+//     data[i].classement = data[i-1].classement;
+//   else
+//     data[i].classement = i+1;
+// }
